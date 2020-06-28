adeb08bc8289e14fdce24ec273758134a6c213ff
jinminho <alsgh4098@naver.com>
2020-06-28 15:27:59 +0900

    commit

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import Weather from "./Weather";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "4fc1d344c9ccf74d5441ae3b6350f2ab";
+
+export default class extends React.Component{
+    state = {
+        isLoading : true
+    };
+
+    getWeather = async (latitude,longitude) =>{
+         const  { data:{
+                         main : {temp},
+                         weather
+                    }
+                } = await axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+                );
+         this.setState({
+             isLoading: false,
+             condition: weather[0].main,
+             temp
+
+         });
+         console.log(data.main.temp);
+    };
+
+    getLocation = async() =>{
+              try{
+                  await Location.requestPermissionsAsync();
+                  const {
+                          coords : { latitude, longitude}
+                  } = await Location.getCurrentPositionAsync();
+                  this.getWeather( latitude,longitude );
+
+              }catch(error){
+                  Alert.alert("Can't find you.", "So sad");
+              }
+    };
+
+    componentDidMount(){
+        this.getLocation();
+    };
+
+    render(){
+        const { isLoading, temp, condition } = this.state;
+        return isLoading ? <Loading/> : <Weather temp={Math.round(temp), condition ={condition} }/>;
+    };
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color: "white"
-  }
-});
